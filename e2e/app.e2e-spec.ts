3d3f39650704f3658966fe5ad32d90b15d9714fc
mattcblack <matthewblackct@gmail.com>
2017-08-13 17:49:00 +0200

    Add files via upload

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+﻿import { MDAManagerPage } from './app.po';
+
+describe('mda.manager App', () => {
+  let page: MDAManagerPage;
+
+  beforeEach(() => {
+    page = new MDAManagerPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
